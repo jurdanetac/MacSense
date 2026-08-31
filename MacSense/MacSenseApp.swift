9547c6c8facac054550a64ef3ca69bd589ebddc8
Juan Urdaneta <jurdanetac10@gmail.com>
2026-08-31 16:33:44 -0400

    Initial Commit

---
+++ MacSense/MacSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSenseApp.swift
+//  MacSense
+//
+//  Created by Juan on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MacSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
